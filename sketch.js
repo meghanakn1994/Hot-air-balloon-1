7d080c302f0fd75ee9c74687f6b0e3e645fad3d0
meghanakn1994 <73427758+meghanakn1994@users.noreply.github.com>
2022-01-12 12:14:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var bg, bgImg
+var bottomGround
+var topGround
+var balloon, balloonImg
+var obstacleTop, obsTop1, obsTop2
+var obstacleBottom, obsBottom1, obsBottom2, obsBottom3
+
+function preload(){
+
+
+}
+
+function setup(){
+
+  createCanvas(400,400)
+//background image
+
+
+
+//creating top and bottom grounds
+
+      
+//creating balloon     
+
+
+
+
+}
+
+function draw() {
+  
+  background("black");
+        
+          //making the hot air balloon jump
+          
+
+          //adding gravity
+           
+       
+        //spawning top obstacles
+     
+      
+}
+
+
+function spawnObstaclesTop() 
+{
+     
+}
+
+ function Bar() 
+ {
+       
+}
+
+
+  
